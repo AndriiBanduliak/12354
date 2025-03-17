efba0903c1979004136405d6543fca86d107340f
Banduliak Andrii <109424920+AndriiBanduliak@users.noreply.github.com>
2025-03-17 11:19:08 +0100

    Delete services/bitget/python_bitget directory

--- services/bitget/python_bitget/examples/example_rest_api.py
+++
@@ -1,9 +0,0 @@
-from pybitget import Client
-
-api_key = "your-api-key"
-api_secret = "your-secret-key"
-api_passphrase = "your-api-passphrase"
-
-client = Client(api_key, api_secret, passphrase=api_passphrase)
-result = client.mix_get_accounts(productType='UMCBL')
-print(result)
